c1b7f66da36227ee887b2105d433c11d55389e20
yiucheung0512 <58731088+yiucheung0512@users.noreply.github.com>
2022-08-23 18:28:39 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from "@splinetool/runtime";
+
+const canvas = document.getElementById("canvas3d");
+const app = new Application(canvas);
+app.load("https://draft.spline.design/30LDOogMlAmJ4KKF/scene.splinecode");
